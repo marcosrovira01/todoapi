43dc8ec9ac5f9280774401d3b6a7db06b426fb54
Marcos <marcosrovira23@gmail.com>
2024-08-18 18:47:31 +0200

    3º Funcionality
    Obtain all tasks by status

--- src/main/java/com/marcthegamer/todoapi/persistence/repository/TaskRepository.java
+++ src/main/java/com/marcthegamer/todoapi/persistence/repository/TaskRepository.java
@@ -1,8 +1,11 @@
 package com.marcthegamer.todoapi.persistence.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.marcthegamer.todoapi.persistence.entity.Task;
+import com.marcthegamer.todoapi.persistence.entity.TaskStatus;
 
 public interface TaskRepository extends JpaRepository<Task, Long>{
-	
+	public List<Task> findAllByTaskStatus(TaskStatus taskStatus);
 }
